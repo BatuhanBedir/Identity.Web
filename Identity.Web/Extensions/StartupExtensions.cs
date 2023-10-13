21b015bea2f694bc167644ca1c47ec84139110c0
Batuhan Bedir <batuhan.bedir133@gmail.com>
2023-10-13 14:32:15 +0300

    resetpassword-mailkit

---
+++ Identity.Web/Extensions/StartupExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Identity.Web.CustomValidations;
+using Identity.Web.Localizations;
+using Identity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Web.Extensions;
+
+public static class StartupExtensions
+{
+    public static void AddIdentityWithExt(this IServiceCollection services)
+    {
+        services.Configure<DataProtectionTokenProviderOptions>(opt => { opt.TokenLifespan = TimeSpan.FromHours(2); });
+        services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyz123456789_";
+
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
+                options.Lockout.MaxFailedAccessAttempts = 3;
+            }).AddErrorDescriber<LocalizationIdentityErrorDescriber>()
+            .AddUserValidator<UserValidator>()
+            .AddPasswordValidator<PasswordValidator>()
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<AppDbContext>();
+    }
+}
---
+++ Identity.Web/ViewModels/ForgetPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Web.ViewModels;
+
+public record ForgetPasswordViewModel
+{
+    [EmailAddress(ErrorMessage = "Email formatı yanlış")]
+    [Display(Name = "Email: ")]
+    [Required(ErrorMessage = "Bu alan boş geçilemez")]
+    public string Email { get; set; } = null!;
+}
